5884ec663ed629b8dd7aaeb78305e7e1c6a003c1
MarekKluk <105215275+MarekKluk@users.noreply.github.com>
2022-05-22 13:57:51 +0200

    yes

--- PracaDomowa3.js
+++ PracaDomowa3.js
@@ -7,10 +7,10 @@ console.log(greeting);
 
 //Zad 2
 const radius=5;
-function getCircleArea(inputRadius) {
+let getArea = function getCircleArea(inputRadius) {
      return Math.pow(inputRadius,2)*Math.PI;
 }
-const area=getCircleArea(radius);
+const area=getArea(radius);
 console.log(area);
 
 //Zad 3
@@ -23,9 +23,7 @@ console.log(radius2);
 
 //Zad 4
 function getCirclesAreaSum (firstRadiusInput, secondRadiusInput) {
-    let firstArea = Math.pow(firstRadiusInput,2)*Math.PI;
-    let secondArea = Math.pow(secondRadiusInput,2)*Math.PI;
-    return firstArea+secondArea;
+    return getArea(firstRadiusInput) + getArea(secondRadiusInput);
 }
 const firstRadius = 5;
 const secondRadius = 10;
@@ -34,10 +32,71 @@ console.log(circlesAreaSum);
 
 //Zad 5 Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
-const quarterOf = (month) => {
-    if(month<=3) {
-        return 1;
-    } else if(month<=6) {
-        return 2;
+const quarterOf = (month) => Math.ceil(month/3);
+const monthNumber=7
+const quarter=quarterOf(monthNumber);
+console.log(quarter);
+
+
+
+//Convert to string function
+function convertToString(bool){
+    let newString = bool === true? 'true':'false';
+    return newString;
+}
+console.log(convertToString(true));
+
+//Write a yesOrNo function that returns ‘Yes’ if the value is truthy and ‘No’ if it is falsy.
+
+function yesOrNo (input){
+    if (input){
+        return 'Yes';
+    } else return 'No';
+}
+console.log(yesOrNo(true));
+console.log(yesOrNo(false));
+console.log(yesOrNo(123));
+console.log(yesOrNo(0));
+
+//Return a string saying how old is the user
+const user = {
+    name: 'John',
+    age: 20
+}
+function howOldIsUser(user) {
+    return user.name + ' is ' + user.age + ' years old';
+}
+console.log(howOldIsUser(user));
+
+//Write the isPalindrome function
+
+function isPalindrome(input){
+    let inputWithoutSpaces = input.replace(/\W/g, "");
+    let newString = '';
+    for (let i = inputWithoutSpaces.length - 1; i>=0; i--) {
+        newString += inputWithoutSpaces[i];
+    }
+    return newString === inputWithoutSpaces? 'True' : "False";
+}
+console.log(isPalindrome('a to kanapa pana kota'));
+
+//Write a rockPaperScissors function that returns 1 if the first player won, and 2 if the second player won. In case of a draw, return 0;
+
+function rockPaperScissors(input1, input2){
+    if (input1 === input2){
+        return '0'
+    } else if(input1 === 'Rock'){
+        if(input2 === 'Scissors'){
+            return '1';
+        }else return'2';
+    }else if (input1 === 'Scissors'){
+        if(input2 === 'Paper'){
+            return '1';
+        } else return '2';
+    }else if (input1 === 'Paper'){
+        if(input2 === 'Rock'){
+            return '1';
+        } else return '2';
     }
 }
+console.log(rockPaperScissors('Paper','Paper'));
